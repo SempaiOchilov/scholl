1e0fe71908ba015125a9fe0189df160f745a2327
SempaiOchilov <139633008+SempaiOchilov@users.noreply.github.com>
2023-10-10 21:22:11 +0500

    Add files via upload

---
+++ school.js
@@ -0,0 +1,21 @@
+
+let a = +prompt("a")
+let b = +prompt("b")
+let c = +prompt("c")
+
+let total = a + b + c
+alert("P равен" + " " + total);
+
+
+
+let aa = +prompt("основание")
+let h = +prompt("высота")
+
+
+let s = 1/2 * aa * h
+
+alert("S треугольника равна  " + " " + s)
+
+
+
+ 
